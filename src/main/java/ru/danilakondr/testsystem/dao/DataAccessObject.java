92b102b19ae6378a971b1485cad39facc50b5c9a
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-10 14:36:56 +0600

    Fix DataAccessObject::get method type

--- src/main/java/ru/danilakondr/testsystem/dao/DataAccessObject.java
+++ src/main/java/ru/danilakondr/testsystem/dao/DataAccessObject.java
@@ -3,6 +3,6 @@ package ru.danilakondr.testsystem.dao;
 public interface DataAccessObject<T, K> {
     void add(T object);
     void delete(T object);
-    void get(K objKey);
+    T get(K objKey);
     void update(T object);
 }
