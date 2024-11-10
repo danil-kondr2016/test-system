d138a681c6dd6bee4e658008c9d0d2b48015c2fb
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-10 14:32:13 +0600

    Add UserSessionDAO interface

---
+++ src/main/java/ru/danilakondr/testsystem/dao/UserSessionDAO.java
@@ -0,0 +1,8 @@
+package ru.danilakondr.testsystem.dao;
+
+import ru.danilakondr.testsystem.data.UserSession;
+
+import java.util.UUID;
+
+public interface UserSessionDAO extends DataAccessObject<UserSession, UUID> {
+}
