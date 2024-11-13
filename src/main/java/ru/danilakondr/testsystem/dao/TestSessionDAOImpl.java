80930fc952a47a81c010059b76e809eab5012ddf
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-13 11:14:37 +0600

    Add TestSessionDAOImpl

---
+++ src/main/java/ru/danilakondr/testsystem/dao/TestSessionDAOImpl.java
@@ -0,0 +1,62 @@
+package ru.danilakondr.testsystem.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.danilakondr.testsystem.data.Participant;
+import ru.danilakondr.testsystem.data.TestSession;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class TestSessionDAOImpl implements TestSessionDAO {
+    private EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Override
+    public List<Participant> getParticipants(TestSession session) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        CriteriaBuilder cb = entityManagerFactory.getCriteriaBuilder();
+        CriteriaQuery<Participant> cq = cb.createQuery(Participant.class);
+        Root<Participant> root = cq.from(Participant.class);
+
+        Predicate _testSessionIdIs = cb.equal(root.get("testSessionId"), session.getTestSessionId());
+        TypedQuery<Participant> query = em.createQuery(cq.where(_testSessionIdIs));
+        return query.getResultList();
+    }
+
+    @Override
+    public void add(TestSession object) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.persist(object);
+    }
+
+    @Override
+    public void delete(TestSession object) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.remove(object);
+    }
+
+    @Override
+    public TestSession get(UUID objKey) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        return em.find(TestSession.class, objKey);
+    }
+
+    @Override
+    public void update(TestSession object) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.refresh(object);
+    }
+}
