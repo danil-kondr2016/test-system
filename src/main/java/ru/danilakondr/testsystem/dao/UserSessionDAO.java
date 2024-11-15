4db7c54024574bfcb74440d24fc1146f2a281d3e
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-15 10:26:56 +0600

    Add UserSessionDAO::getAllUserSessions

--- src/main/java/ru/danilakondr/testsystem/dao/UserSessionDAO.java
+++ src/main/java/ru/danilakondr/testsystem/dao/UserSessionDAO.java
@@ -2,7 +2,10 @@ package ru.danilakondr.testsystem.dao;
 
 import ru.danilakondr.testsystem.data.UserSession;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 public interface UserSessionDAO extends DataAccessObject<UserSession, UUID> {
+    Stream<UserSession> getAllUserSessions(UserSession auth);
 }
--- src/main/java/ru/danilakondr/testsystem/dao/UserSesssionDAOImpl.java
+++ src/main/java/ru/danilakondr/testsystem/dao/UserSesssionDAOImpl.java
@@ -2,11 +2,15 @@ package ru.danilakondr.testsystem.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import ru.danilakondr.testsystem.data.User;
 import ru.danilakondr.testsystem.data.UserSession;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Repository
 public class UserSesssionDAOImpl implements UserSessionDAO {
@@ -39,5 +43,20 @@ public class UserSesssionDAOImpl implements UserSessionDAO {
     public void update(UserSession object) {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.merge(object);
+    }
+
+    @Override
+    public Stream<UserSession> getAllUserSessions(UserSession auth) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        // Check if user exists and is administrator
+        TypedQuery<User> userQuery = em.createQuery("FROM User WHERE userId=:id", User.class);
+        userQuery.setParameter("id", auth.getUserId());
+        User user = userQuery.getSingleResult();
+        if (user.getUserRole() != User.Role.ADMINISTRATOR)
+            throw new RuntimeException("Permission denied");
+
+        TypedQuery<UserSession> userSessionQuery = em.createQuery("FROM UserSession", UserSession.class);
+        return userSessionQuery.getResultStream();
     }
 }
