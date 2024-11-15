f4cc1f8e0fd8d28248620cb82d019c2c9af1bc7f
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-15 10:27:10 +0600

    Add UserDAO::getByLogin and UserDAO::getTestSessions

--- src/main/java/ru/danilakondr/testsystem/dao/UserDAO.java
+++ src/main/java/ru/danilakondr/testsystem/dao/UserDAO.java
@@ -1,10 +1,13 @@
 package ru.danilakondr.testsystem.dao;
 
 import ru.danilakondr.testsystem.data.Test;
+import ru.danilakondr.testsystem.data.TestSession;
 import ru.danilakondr.testsystem.data.User;
 
 import java.util.List;
 
 public interface UserDAO extends DataAccessObject<User, Long> {
+    User getByLogin(String login);
     List<Test> getTests(User user);
+    List<TestSession> getTestSessions(User user);
 }
--- src/main/java/ru/danilakondr/testsystem/dao/UserDAOImpl.java
+++ src/main/java/ru/danilakondr/testsystem/dao/UserDAOImpl.java
@@ -2,6 +2,7 @@ package ru.danilakondr.testsystem.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -10,6 +11,7 @@ import jakarta.persistence.criteria.Root;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.danilakondr.testsystem.data.Test;
+import ru.danilakondr.testsystem.data.TestSession;
 import ru.danilakondr.testsystem.data.User;
 
 import java.util.List;
@@ -24,6 +26,23 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
+    public User getByLogin(String login) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        CriteriaBuilder cb = entityManagerFactory.getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+        Root<User> root = cq.from(User.class);
+
+        Predicate _userIdIs = cb.equal(root.get("login"), login);
+        TypedQuery<User> query = em.createQuery(cq.where(_userIdIs));
+        try {
+            return query.getSingleResult();
+        }
+        catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    @Override
     public List<Test> getTests(User user) {
         EntityManager em = entityManagerFactory.createEntityManager();
         CriteriaBuilder cb = entityManagerFactory.getCriteriaBuilder();
@@ -32,6 +51,18 @@ public class UserDAOImpl implements UserDAO {
 
         Predicate _organizerIdIs = cb.equal(root.get("organizatorId"), user.getUserId());
         TypedQuery<Test> query = em.createQuery(cq.where(_organizerIdIs));
+        return query.getResultList();
+    }
+
+    @Override
+    public List<TestSession> getTestSessions(User user) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        TypedQuery<TestSession> query = em.createQuery(
+                "SELECT s FROM Test t INNER JOIN TestSession t.testId s WHERE t.userId=:id",
+                TestSession.class
+        );
+        query.setParameter("id", user.getUserId());
+
         return query.getResultList();
     }
 
