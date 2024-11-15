844279fe03e27852b39f360c7b84ebef0afe1505
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-15 10:26:43 +0600

    Add TestSessionDAO::getAllActiveTestSessions

--- src/main/java/ru/danilakondr/testsystem/dao/TestSessionDAO.java
+++ src/main/java/ru/danilakondr/testsystem/dao/TestSessionDAO.java
@@ -2,10 +2,13 @@ package ru.danilakondr.testsystem.dao;
 
 import ru.danilakondr.testsystem.data.TestSession;
 import ru.danilakondr.testsystem.data.Participant;
+import ru.danilakondr.testsystem.data.UserSession;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 public interface TestSessionDAO extends DataAccessObject<TestSession, UUID> {
     List<Participant> getParticipants(TestSession session);
+    Stream<TestSession> getAllActiveTestSessions(UserSession auth);
 }
--- src/main/java/ru/danilakondr/testsystem/dao/TestSessionDAOImpl.java
+++ src/main/java/ru/danilakondr/testsystem/dao/TestSessionDAOImpl.java
@@ -11,9 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.danilakondr.testsystem.data.Participant;
 import ru.danilakondr.testsystem.data.TestSession;
+import ru.danilakondr.testsystem.data.User;
+import ru.danilakondr.testsystem.data.UserSession;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Repository
 public class TestSessionDAOImpl implements TestSessionDAO {
@@ -34,6 +37,23 @@ public class TestSessionDAOImpl implements TestSessionDAO {
         Predicate _testSessionIdIs = cb.equal(root.get("testSessionId"), session.getTestSessionId());
         TypedQuery<Participant> query = em.createQuery(cq.where(_testSessionIdIs));
         return query.getResultList();
+    }
+
+    @Override
+    public Stream<TestSession> getAllActiveTestSessions(UserSession auth) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        // Check if user exists and is administrator
+        TypedQuery<User> userQuery = em.createQuery("FROM User WHERE userId=:id", User.class);
+        userQuery.setParameter("id", auth.getUserId());
+        User user = userQuery.getSingleResult();
+        if (user.getUserRole() != User.Role.ADMINISTRATOR)
+            throw new RuntimeException("Permission denied");
+
+        TypedQuery<TestSession> testSessionQuery = em.createQuery(
+                "FROM TestSession WHERE testSessionState=:state",
+                TestSession.class).setParameter("state", TestSession.State.ACTIVE);
+        return testSessionQuery.getResultStream();
     }
 
     @Override
