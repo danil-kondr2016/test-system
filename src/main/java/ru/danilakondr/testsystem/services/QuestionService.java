492d60648d10941dbcb9dc1efac92644e659ab8e
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-18 09:58:06 +0600

    Change QuestionService::create

--- src/main/java/ru/danilakondr/testsystem/services/QuestionService.java
+++ src/main/java/ru/danilakondr/testsystem/services/QuestionService.java
@@ -3,7 +3,7 @@ package ru.danilakondr.testsystem.services;
 import ru.danilakondr.testsystem.data.Question;
 
 public interface QuestionService {
-    Question create(long testId);
+    Question create(long testId, Question.Type type, String text);
     Question get(long questionId);
     void update(Question question);
     void remove(long questionId);
