cce7c471efe732af1f5724d1ad27f7e228072cef
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-18 10:19:51 +0600

    Change TestSessionService interface

--- src/main/java/ru/danilakondr/testsystem/services/TestSessionService.java
+++ src/main/java/ru/danilakondr/testsystem/services/TestSessionService.java
@@ -2,14 +2,15 @@ package ru.danilakondr.testsystem.services;
 
 import ru.danilakondr.testsystem.data.Test;
 import ru.danilakondr.testsystem.data.TestSession;
+import ru.danilakondr.testsystem.data.UserSession;
 import ru.danilakondr.testsystem.info.Report;
 
 import java.util.UUID;
 
 public interface TestSessionService {
-    TestSession create(Test test);
+    TestSession create(UserSession auth, long testId);
     TestSession get(UUID testSessionId);
     void update(TestSession testSession);
     void remove(UUID testSessionId);
-    Report loadReport(TestSession testSession, long participantId);
+    Report loadReport(long participantId);
 }
