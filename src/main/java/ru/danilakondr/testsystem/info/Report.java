79e2ccc86bb0fe490df1395d77d113f28308232c
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-18 10:02:22 +0600

    Implement class Report

--- src/main/java/ru/danilakondr/testsystem/info/Report.java
+++ src/main/java/ru/danilakondr/testsystem/info/Report.java
@@ -1,5 +1,43 @@
 package ru.danilakondr.testsystem.info;
 
-// TODO implement
+import java.util.ArrayList;
+import java.util.List;
+
 public class Report {
+    public record Answer(long testId,
+                         long questionId,
+                         String questionText,
+                         long answerVariantId,
+                         String answerVariantText,
+                         boolean correct) {}
+
+    private final long participantId;
+    private final String participantName;
+    private final List<Answer> answers;
+
+    public Report(long participantId, String participantName) {
+        answers = new ArrayList<>();
+        this.participantId = participantId;
+        this.participantName = participantName;
+    }
+
+    public long getParticipantId() {
+        return participantId;
+    }
+
+    public String getParticipantName() {
+        return participantName;
+    }
+
+    public Answer getAnswer(int index) {
+        return answers.get(index);
+    }
+
+    public void putAnswer(Answer answer) {
+        answers.add(answer);
+    }
+
+    public int getAnswerCount(int index) {
+        return answers.size();
+    }
 }
