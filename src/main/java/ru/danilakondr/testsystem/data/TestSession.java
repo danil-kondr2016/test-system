6938f8b404b1f2b356e1f1da1bc732e3ac9a6ff6
Danila Kondratenko <danilakondratenko95@gmail.com>
2024-12-02 13:06:42 +0000

    Lombokize TestSession

--- src/main/java/ru/danilakondr/testsystem/data/TestSession.java
+++ src/main/java/ru/danilakondr/testsystem/data/TestSession.java
@@ -2,11 +2,19 @@ package ru.danilakondr.testsystem.data;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestSession {
     @JsonFormat(shape=JsonFormat.Shape.STRING)
     public enum State {
@@ -26,44 +34,4 @@ public class TestSession {
     private LocalDateTime begin;
 
     private LocalDateTime end;
-
-    public UUID getTestSessionId() {
-        return testSessionId;
-    }
-
-    public long getTestId() {
-        return testId;
-    }
-
-    public State getTestSessionState() {
-        return testSessionState;
-    }
-
-    public LocalDateTime getBegin() {
-        return begin;
-    }
-
-    public LocalDateTime getEnd() {
-        return end;
-    }
-
-    public void setTestSessionId(UUID testSessionId) {
-        this.testSessionId = testSessionId;
-    }
-
-    public void setTestId(long testId) {
-        this.testId = testId;
-    }
-
-    public void setTestSessionState(State testSessionState) {
-        this.testSessionState = testSessionState;
-    }
-
-    public void setBegin(LocalDateTime begin) {
-        this.begin = begin;
-    }
-
-    public void setEnd(LocalDateTime end) {
-        this.end = end;
-    }
 }
