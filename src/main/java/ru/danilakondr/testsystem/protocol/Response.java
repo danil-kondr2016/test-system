caf54ca5211ca980e92f45e2e6b210373e7e0ce7
Danila Kondratenko <danilakondratenko95@gmail.com>
2024-12-03 15:23:09 +0000

    Replace "JWT" with "SESSION_KEY"

--- src/main/java/ru/danilakondr/testsystem/protocol/Response.java
+++ src/main/java/ru/danilakondr/testsystem/protocol/Response.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, property="response_type", visible = true)
 @JsonSubTypes({
-          @JsonSubTypes.Type(value= SessionKeyResponse.class, name="JWT")
+          @JsonSubTypes.Type(value= SessionKeyResponse.class, name="SESSION_KEY")
         , @JsonSubTypes.Type(value= ErrorResponse.class, name="ERROR")
 })
 public abstract class Response {
